3981602cd981f78faf19ebc9486a8622a2b4947a
Daniil_Klyukin <anatoliikljukin@mail.ru>
2025-12-30 23:25:08 +0400

    Add F solution

---
+++ ICPC/Финалы/2025/F.Fragmented Nim/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace F.Fragmented_Nim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = int.Parse(Console.ReadLine());
+
+            var games = new int[t][];
+
+            for (int i = 0; i < t; i++)
+            {
+                Console.ReadLine();
+
+                games[i] = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            }
+
+            foreach (var game in games)
+            {
+                Console.WriteLine(GetWinner(game));
+            }
+        }
+
+        public static string GetWinner(int[] game)
+        {
+            var notOneCount = game.Count(x => x != 1);
+            var isCountOdd = game.Length % 2 == 1;
+
+            if (notOneCount == 0)
+            {
+                return isCountOdd ? "Alice" : "Bob";
+            }
+
+            var isNotOneCountEven = notOneCount % 2 == 0;
+
+            return (isCountOdd == isNotOneCountEven) ? "Bob" : "Alice";
+        }
+    }
+}
